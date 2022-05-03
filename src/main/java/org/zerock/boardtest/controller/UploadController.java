30527fc99492e88eaf37a1ea2c94e7dd69fdd235
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2022-05-03 13:40:54 +0900

    file upload json load

--- src/main/java/org/zerock/boardtest/controller/UploadController.java
+++ src/main/java/org/zerock/boardtest/controller/UploadController.java
@@ -1,18 +1,23 @@
 package org.zerock.boardtest.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.boardtest.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -20,32 +25,68 @@ import java.util.UUID;
 public class UploadController {
 
     @PostMapping("/upload1")
-    public void upload1(MultipartFile[] files){
-
+    @ResponseBody
+    public List<UploadResultDTO> upload1(MultipartFile[] files) {
         log.info("==================================");
 
-       log.info(files);
+        log.info(files);
 
-       //업로드된 파일 있다고 가정
-        for (MultipartFile file:files) {
+        List<UploadResultDTO> list = new ArrayList<>();
+
+        //업로드된 파일 있다고 가정
+        for (MultipartFile file : files) {
 
             String originalFileName = file.getOriginalFilename();
 
-            String saveName = UUID.randomUUID().toString()+"_"+originalFileName;
+            boolean img = file.getContentType().startsWith("image");
+
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uuid + "_" + originalFileName;
 
             log.info(file.getResource());
             String saveFolder = makeFolders();
 
-            try(InputStream in = file.getInputStream();
-                FileOutputStream fos = new FileOutputStream("C:\\upload\\"+saveFolder+"\\"+saveName);
-                ){FileCopyUtils.copy(in,fos);
+            File savaFile = new File("C:\\upload\\" + saveFolder + "\\" + saveName);
+
+            try (InputStream in = file.getInputStream();
+                 FileOutputStream fos = new FileOutputStream(savaFile);
+            ) {
+                FileCopyUtils.copy(in, fos);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+            if (img) {
+                //saveNmae = UUID+"_"+fileName
+                String thumbFileName = saveFolder + "\\s_" + saveName;
+                File thumFile = new File("C:\\upload\\" + thumbFileName);
+
+                try {
+                    Thumbnails.of(savaFile)
+                            .size(200, 200)
+                            .toFile(thumFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            list.add(UploadResultDTO.builder()
+                    .original(originalFileName)
+                    .uuid(uuid)
+                    .img(img)
+                    .savePath(saveFolder)
+                    .build());
+
+            log.info("-------------------------------------");
+
         }//emd for
+
+        return list;
     }
-    private String makeFolders(){
+
+    private String makeFolders() {
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
@@ -53,7 +94,7 @@ public class UploadController {
 
         File folderPath = new File("C:\\upload\\" + str);
 
-        if(!folderPath.exists()){
+        if (!folderPath.exists()) {
             folderPath.mkdirs();
         }
 
---
+++ src/main/java/org/zerock/boardtest/dto/UploadResultDTO.java
@@ -0,0 +1,19 @@
+package org.zerock.boardtest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadResultDTO {
+
+    private String uuid;
+    private String original;
+    private String savePath;
+    private boolean img;
+
+}

3475394fb9a59b9960d71f561fd76349b762fe8c
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2022-05-17 11:30:14 +0900

    login annotation

--- src/main/java/org/zerock/boardtest/controller/SampleController.java
+++ src/main/java/org/zerock/boardtest/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.boardtest.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,13 @@ public class SampleController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_MEMBER', 'ROLE_ADMIN')")
     @GetMapping("member")
     public void doMember(){
 
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("admin")
     public void doAdmin(){
 
